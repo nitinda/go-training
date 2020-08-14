07dbeecbee688644a7826674ed521009133c2fe1
nitinda <nitinda@gmail.com>
2020-08-14 10:38:10 +0000

    Modified

---
+++ goroutines/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+var counter = 0
+var m = sync.RWMutex{} // Read Write mutex
+
+func main() {
+	runtime.GOMAXPROCS(20)
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+
+		m.RLock()
+		go sayHello()
+
+		m.Lock()
+		go increment()
+	}
+	wg.Wait()
+}
+
+func sayHello() {
+	fmt.Println("Hello  #", counter)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
+}
