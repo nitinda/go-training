3060c9dd9f244d08cd9e284859b96229249192dd
Nitin Das <37865475+nitinda@users.noreply.github.com>
2021-06-28 22:37:33 +0100

    Added slices

---
+++ slices/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+func main() {
+	scores := make([]int, 100)
+	for i := 0; i < 100; i++ {
+		scores[i] = int(rand.Int31n(1000))
+	}
+
+	sort.Ints(scores)
+
+	fmt.Println(scores)
+
+	worst := make([]int, 5)
+	copy(worst, scores[5:20])
+
+	worst[1] = 9000
+
+	fmt.Println(worst)
+	fmt.Println(scores)
+}
